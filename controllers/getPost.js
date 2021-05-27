0d69342c9271b27b5e2d629c998ca6237db2297d
wnwoghd22 <53481297+wnwoghd22@users.noreply.github.com>
2021-05-28 00:37:53 +0900

    Fixing
    잠재적 수정사항들을 찾아냈습니다.
    자세한 내용은 회의에서 고칩시다.

--- controllers/getPost.js
+++ controllers/getPost.js
@@ -1,8 +1,10 @@
 const BlogPost = require('../models/BlogPost.js')
 module.exports = async (req,res) =>{
     const blogpost =  await BlogPost.findById(req.params.id).populate('userid')
+    const userInfo = req.session.User;
     // console.log(blogpost)
     res.render('post',{
+        userInfo,
         blogpost
     })
 }
--- controllers/home.js
+++ controllers/home.js
@@ -1,9 +1,11 @@
 const BlogPost = require('../models/BlogPost.js')
 
 module.exports = async (req, res) =>{
-    const blogposts = await BlogPost.find({}).populate('userid')  
+    const blogposts = await BlogPost.find({}).populate('userid');
+    const userInfo = req.session.User; //페이지 내 user 정보 표시를 위한 패러미터
     // console.log(req.session)      
     res.render('index',{
+        userInfo,
         blogposts
     });
 }
