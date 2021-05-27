46a3c183cc96d7181aff2c10670841a0a0fe13b0
pavk96 <rbwjd97@naver.com>
2021-05-28 08:53:33 +0900

    Revert "Fixing"
    This reverts commit 0d69342c9271b27b5e2d629c998ca6237db2297d.

--- controllers/loginUser.js
+++ controllers/loginUser.js
@@ -7,16 +7,8 @@ module.exports = (req, res) =>{
     User.findOne({username:username}, (error,user) => {      
       if (user){           
         bcrypt.compare(password, user.password, (error, same) =>{          
-          if(same){
-            req.session.userId=user._id
-            /**
-             * 이용자 정보 표시를 위한 필드 추가.
-             * 필드 추가 시 윗줄의 id를 별도로 저장할 필요는 없습니다만,
-             * 다른 파일에서 수정할 부분이 생기니 일단 다음 회의때까지 두겠습니다
-             */
-            req.session.User = user;
-            console.log(user);
-            
+          if(same){ 
+            req.session.userId=user._id            
             res.redirect('/')
           }
           else{
