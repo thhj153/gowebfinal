2f15692839b93a6c4d8b8129032264cd6d618792
pavk96 <rbwjd97@naver.com>
2021-06-01 10:54:48 +0900

    usergrade manage issue 완료

--- controllers/updateUserList.js
+++ controllers/updateUserList.js
@@ -1,10 +1,11 @@
 const User = require("../models/User");
 
 module.exports = async (req, res) => {
-  console.log(req.userList);
+  console.log(req.body.userList);
   const userList = req.body.userList;
 
   for (let i = 0; i < userList.length; i += 2) {
-    await User.updateOne({ name: userList[i] }, { grade: userList[i] });
+    await User.updateOne({ _id: userList[i] }, { grade: userList[i + 1] });
   }
+  res.redirect("/userlist");
 };
