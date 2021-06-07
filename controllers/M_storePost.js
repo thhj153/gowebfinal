70d5a14992cf5c4b88891cc5b77ba6161e615b32
wnwoghd22 <53481297+wnwoghd22@users.noreply.github.com>
2021-06-07 20:31:10 +0900

    post without image
    사진 없이 글 올리기 기능 개선

--- controllers/M_storePost.js
+++ controllers/M_storePost.js
@@ -2,24 +2,37 @@ const BlogPost = require("../models/BlogPost.js");
 const path = require("path");
 
 module.exports = (req, res) => {
-  let image = req.files.image;
+  let image = req.files != null ? req.files.image : null;
 
-  image.mv(
-    path.resolve(__dirname, "..", "public/img", image.name),
-    async (error) => {
-      req.body.notice;
-      let noticeCheck = false;
-      if (req.body.notice === "on") {
-        noticeCheck = true;
+  if(image != null) {
+    image.mv(
+      path.resolve(__dirname, "..", "public/img", image.name),
+      async (error) => {
+        let noticeCheck = false;
+        if (req.body.notice === "on") {
+          noticeCheck = true;
+        }
+        await BlogPost.create({
+          ...req.body,
+          image: "/img/" + image.name,
+          userid: req.session.userId,
+          category: req.body.category,
+          notice: noticeCheck,
+        });
+        res.redirect("/");
       }
-      await BlogPost.create({
-        ...req.body,
-        image: "/img/" + image.name,
-        userid: req.session.userId,
-        category: req.body.category,
-        notice: noticeCheck,
-      });
-      res.redirect("/");
+    );
+  } else {
+    let noticeCheck = false;
+    if (req.body.notice === "on") {
+      noticeCheck = true;
     }
-  );
+    BlogPost.create({
+      ...req.body,
+      userid: req.session.userId,
+      category: req.body.category,
+      notice: noticeCheck,
+    });
+    res.redirect("/");
+  }
 };
--- middleware/validateMiddleware.js
+++ middleware/validateMiddleware.js
@@ -1,5 +1,5 @@
 module.exports = (req,res,next)=>{    
-    if(req.files == null || req.body.title == null || req.body.body == null){        
+    if(/*req.files == null ||*/ req.body.title == null || req.body.body == null){        
         return res.redirect('/posts/new')
     }    
     next()
