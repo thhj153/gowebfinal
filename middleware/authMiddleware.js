c75745fbe64be6f2ff9897839cb9e85f23e31f17
thhj153 <44903440+thhj153@users.noreply.github.com>
2021-06-13 06:12:36 +0900

    UX update
    1. Move to Login; If not logged in.
    2. Make path; Between register and login.
    3. Auto-Background; If any background is registed(4 diffrent pictures).
    4. Counting articles; Including article doesn't exist.
    5. some UIs updated.

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,7 +2,7 @@ const User = require("../models/User");
 
 module.exports = (req, res, next) => {
   User.findById(req.session.userId, (error, user) => {
-    if (error || !user) return res.redirect("/");
+    if (error || !user) return res.redirect("/user/auth/login");
     next();
   });
 };
